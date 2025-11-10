6d4fa7d106f50a2df39f92bd229f50d461172b93
lhbecalli <luizhenriquebecallidefaria@gmail.com>
2025-11-10 12:29:37 -0300

    New animations update

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+<<<<<<< HEAD
 const prevButton = document.getElementById("esquerda")
 const nextButton = document.getElementById("direita")
 const itens = document.querySelectorAll('.item')
 const list = document.querySelector('.list')
 const dots = document.querySelectorAll('.number-dot') 
+=======
+const nextButton = document.querySelector('.arrow-right')
+const previewButton = document.querySelector('.arrow-left')
+const itens = document.querySelectorAll('.item')
+const lista = document.querySelector('.list')
+const dots = document.querySelectorAll('.number-dot')
+>>>>>>> b5da7ac (New animations update)
 
 
 let active = 0;
 const total = itens.length
 
+<<<<<<< HEAD
 
 function update(direction){
 
@@ -44,10 +53,39 @@ function update(direction){
     // Adiciona a classe ACTIVE ao próximo item
     itens[active].classList.add('active')
     dots[active].classList.add('active')
+=======
+function update(direction){
+
+
+    // Vai procurar um item q tem a classe item e a classe active
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.number-dot.active').classList.remove('active')
+
+    
+        // Agora devo saber qual é a direção.
+        if(direction > 0){
+            active = active + 1;
+            if(active == total){
+                active = 0;
+            }
+        }
+        else if(direction < 0){
+            active = active - 1
+       if(active < 0){
+            active = total - 1;
+       }}
+            
+            //Pega o Item na classe active e
+        itens[active].classList.add('active')
+        dots[active].classList.add('active')
+
+     
+>>>>>>> b5da7ac (New animations update)
 
 }
 
 
+<<<<<<< HEAD
 
 
 nextButton.addEventListener('click', () => {
@@ -60,4 +98,12 @@ prevButton.addEventListener('click', () => {
 
     // Vai executar a função definida abaixo. 'Update'
    update(1)
+=======
+previewButton.addEventListener('click', () => {
+    update(1)
+}) 
+
+nextButton.addEventListener('click', () => {
+    update(-1)
+>>>>>>> b5da7ac (New animations update)
 })
